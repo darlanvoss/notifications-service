b70beac51ccf9482fc6336b26fb8d540ab410a41
Darlan Voss <darlanvoss@gmail.com>
2022-12-28 15:08:42 -0300

    Initial commit

---
+++ src/app.controller.ts
@@ -0,0 +1,28 @@
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
+import { randomUUID } from 'node:crypto';
+import { createNotificationBody } from './create-notification-bady';
+
+@Controller('notifications')
+export class AppController {
+  constructor(private readonly prisma: PrismaService) {}
+
+  @Get()
+  getHello() {
+    return this.prisma.notification.findMany();
+  }
+
+  @Post()
+  async create(@Body() body: createNotificationBody) {
+    const { recipientId, content, category } = body;
+
+    await this.prisma.notification.create({
+      data: {
+        id: randomUUID(),
+        content,
+        category,
+        recipientId,
+      },
+    });
+  }
+}
